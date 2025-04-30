31a4926c801be69ad27ecaa2b5588469d9e5ca63
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-30 14:12:52 +0530

    fix: fix bug related to radio buttons

--- src/qiuzForm/quizForm.ts
+++ src/qiuzForm/quizForm.ts
@@ -8,24 +8,24 @@ export function induceMainQuizForm(mainElement, index, player) {
         <form class="form-element">
         <h3 class="question">Question ${index}: ${questionBody.question}</h3>
 
-            <div class = 'option-label-wrapper'>
-                <input type="radio" class="option1" id = 'option1' value='${questionBody.correct_answer}'/>
-                <label for = 'option1'> ${questionBody.correct_answer} </label>
+            <div class = "option-label-wrapper">
+                <input type="radio" id = "option1" name = "quiz-option" value="${questionBody.correct_answer}"/>
+                <label for = "option1"> ${questionBody.correct_answer} </label>
             </div>
 
-            <div class = 'option-label-wrapper'>
-                <input type="radio" class="option2" id = 'option2' value='${questionBody.incorrect_answers[0]}' />
-                <label for = 'option2'> ${questionBody.incorrect_answers[0]} </label>
+            <div class = "option-label-wrapper">
+                <input type="radio" id = "option2" name = "quiz-option" value="${questionBody.incorrect_answers[0]}" />
+                <label for = "option2"> ${questionBody.incorrect_answers[0]} </label>
             </div>
 
-            <div class = 'option-label-wrapper'>
-                <input type="radio" class="option3" id = 'option3' value='${questionBody.incorrect_answers[1]}'/>
-                <label for = 'option3'> ${questionBody.incorrect_answers[1]} </label>
+            <div class = "option-label-wrapper">
+                <input type="radio" id = "option3" name = "quiz-option" value="${questionBody.incorrect_answers[1]}"/>
+                <label for = "option3"> ${questionBody.incorrect_answers[1]} </label>
             </div>
 
-            <div class = 'option-label-wrapper'>
-                <input type="radio" class="option4" id = 'option4' value='${questionBody.incorrect_answers[2]}'/>
-                <label for = 'option4'> ${questionBody.incorrect_answers[2]} </label>
+            <div class = "option-label-wrapper">
+                <input type="radio" name = "quiz-option" id = "option4" value="${questionBody.incorrect_answers[2]}"/>
+                <label for = "option4"> ${questionBody.incorrect_answers[2]} </label>
             </div>
             </form>
   
