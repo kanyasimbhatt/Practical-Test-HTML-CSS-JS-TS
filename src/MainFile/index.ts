4c0a0e8b15e58b92594df73dfc752b80e3cec723
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-30 13:20:40 +0530

    fix: fix the logic for timer

--- src/MainFile/index.ts
+++ src/MainFile/index.ts
@@ -1,17 +1,26 @@
 import { induceNavbar } from "../Navbar/navbar";
 import { induceMainQuizForm } from "../qiuzForm/quizForm";
+import { induceAnalysisSection } from "../AnalysisSection/analysisSection";
 
 document.addEventListener("DOMContentLoaded", () => {
   const navbarElement = document.getElementsByClassName("navbar-div")[0];
   const mainQuizFormElement =
-    document.getElementsByClassName("quiz-timer-form")[0];
+    document.getElementsByClassName("quiz-form-wrapper")[0];
+  const analysisSectionElement = document.getElementsByClassName(
+    "quiz-player-turn-stats-timer"
+  )[0];
 
   induceNavbar(navbarElement);
+  induceMainQuizForm(mainQuizFormElement, 1, 1);
+  let questionPlayer1 = 2;
+  let questionPlayer2 = 1;
 
-  let timer = 10;
-  let interval = setInterval(() => {
-    if (timer === 1) clearInterval(interval);
-    timer--;
-    induceMainQuizForm(mainQuizFormElement, 1, 1, timer);
-  }, 1000);
+  for (let i = 0; i < 20; i++) {
+    let timer = 10;
+    let interval = setInterval(() => {
+      if (timer === 1) clearInterval(interval);
+      timer--;
+      induceAnalysisSection(analysisSectionElement, 1, timer);
+    }, 1000);
+  }
 });
--- src/qiuzForm/quizForm.ts
+++ src/qiuzForm/quizForm.ts
@@ -1,42 +1,35 @@
 import { getQuestionBodyForPlayer } from "../DataHandle/dataHandle";
 import { player1Analysis, player2Analysis } from "../DataHandle/dataStore";
 
-export function induceMainQuizForm(mainElement, index, player, timer) {
+export function induceMainQuizForm(mainElement, index, player) {
   let questionBody = getQuestionBodyForPlayer(player, index);
   mainElement.innerHTML = `
-    <div class="quiz-player-turn-stats-timer">
-        <p>Player1 Got Correct: ${player1Analysis.CorrectlyAnswered}</p>
-        <p>Question Passed so far: ${player1Analysis.questionsPassed}</p>
-        <p>Player2 Got Correct: ${player2Analysis.CorrectlyAnswered}</p>
-        <p>Question Passed so far: ${player2Analysis.questionsPassed}</p>
-        <p>Player Turn: Player${index}</p>
-        <p>Timer: ${timer}</p>
-      </div>
+   
 
-      <div class="quiz-form-wrapper">
+
         <div class="question">Question ${index}: ${questionBody.question}</div>
 
-        <div class="options">
+        <form class="options">
             <div class = 'option-label-wrapper'>
                 <input type="radio" class="option" id = 'option1'/>
                 <label for = 'option1'> ${questionBody.correct_answer} </label>
             </div>
 
             <div class = 'option-label-wrapper'>
-                <input type="radio" class="option" id = 'option1'/>
-                <label for = 'option1'> ${questionBody.incorrect_answers[0]} </label>
+                <input type="radio" class="option" id = 'option2'/>
+                <label for = 'option2'> ${questionBody.incorrect_answers[0]} </label>
             </div>
 
             <div class = 'option-label-wrapper'>
-                <input type="radio" class="option" id = 'option1'/>
-                <label for = 'option1'> ${questionBody.incorrect_answers[1]} </label>
+                <input type="radio" class="option" id = 'option3'/>
+                <label for = 'option3'> ${questionBody.incorrect_answers[1]} </label>
             </div>
 
             <div class = 'option-label-wrapper'>
-                <input type="radio" class="option" id = 'option1'/>
-                <label for = 'option1'> ${questionBody.incorrect_answers[2]} </label>
+                <input type="radio" class="option" id = 'option4'/>
+                <label for = 'option4'> ${questionBody.incorrect_answers[2]} </label>
             </div>
-            
-        </div>
-      </div>`;
+            </form>
+  
+      `;
 }
