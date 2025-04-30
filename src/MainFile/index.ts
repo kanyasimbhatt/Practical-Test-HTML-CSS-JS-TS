305c3f6040626be01ea28257c1d5c09b83e86af9
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-30 12:08:26 +0530

    fix: fix the naming mistake

--- src/MainFile/index.ts
+++ src/MainFile/index.ts
@@ -3,9 +3,9 @@ import { induceMainQuizForm } from "../qiuzForm/quizForm";
 
 document.addEventListener("DOMContentLoaded", () => {
   const navbarElement = document.getElementsByClassName("navbar-div")[0];
-  const mainquizFormElement =
+  const mainQuizFormElement =
     document.getElementsByClassName("quiz-timer-form")[0];
 
   induceNavbar(navbarElement);
-  induceMainQuizForm(mainquizFormElement);
+  induceMainQuizForm(mainQuizFormElement);
 });
