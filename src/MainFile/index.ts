99bee376c825e26327736b7cba53132f14a772c1
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-30 12:06:16 +0530

    add: add basic UI layout for quiz application

--- src/MainFile/index.ts
+++ src/MainFile/index.ts
@@ -1,6 +1,11 @@
 import { induceNavbar } from "../Navbar/navbar";
+import { induceMainQuizForm } from "../qiuzForm/quizForm";
 
 document.addEventListener("DOMContentLoaded", () => {
   const navbarElement = document.getElementsByClassName("navbar-div")[0];
+  const mainquizFormElement =
+    document.getElementsByClassName("quiz-timer-form")[0];
+
   induceNavbar(navbarElement);
+  induceMainQuizForm(mainquizFormElement);
 });
